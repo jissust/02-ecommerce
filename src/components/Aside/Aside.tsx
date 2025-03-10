9487311d54b84326c45de27c83c8bc32c87c928f
jesustissera <jesustissera@gmail.com>
2025-03-10 20:36:03 -0300

    added first changes in aside block

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,7 +1,31 @@
 function Aside(){
     return (
-        <section className="w-[400px] bg-blue-100 h-[100vh]">
-            Aside
+        <section className="w-[20%] bg-blue-100 h-[100vh] p-5">
+            <ul>
+                <li className="pb-5">
+                    <div>
+                    Precio:
+                    </div>
+                    <div className="flex gap-4">
+                        <div>Precio máximo:</div>
+                        <input type="text" className="border" /> 
+                    </div>
+                </li>
+                <li className="pb-5">
+                    <div>Categoría:</div>
+                    <ul>
+                        <li>
+                            Categoría 1
+                        </li>
+                        <li>
+                            Categoría 2
+                        </li>
+                        <li>
+                            Categoría 3
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </section>
     )
 }
