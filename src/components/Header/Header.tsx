0174039bf27202a4c1fc87fe1eb7d305449bcb9f
jesustissera <jesustissera@gmail.com>
2025-03-10 14:23:28 -0300

    Responsive version of the navigation bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,36 @@ import { useState } from "react";
 
 function Header() {
   const [toogleHidden, setToogleHidden] = useState(true);
+  const [hideMenu, setHideMenu] = useState(true);
+
   const toogleBlock = () => {
     setToogleHidden((prevState) => !prevState);
-  };
+  }
+
+  const showHideMenu = () => {
+    setHideMenu((prevState) => !prevState);
+    console.log(hideMenu)
+  }
+
   return (
-    <nav className="w-full h-[80px] bg-sky-500 text-white">
-      <div className="container flex mx-auto h-full">
-        <div className="grow-3 grid items-center">Logo</div>
-        <div className="grow-7 grid items-center">
+    <nav className="w-full sm:h-[80px] bg-sky-500 text-white">
+      <div className="container block sm:flex mx-auto h-full p-5 sm:p-0">
+        <div className="relative grow grid items-center justify-center sm:justify-start">
+          Logo
+          <button className="absolute top-0 right-0 px-5 block sm:hidden" onClick={showHideMenu}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              width="20"
+            >
+              <path
+                fill="white"
+                d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className={`grow items-center py-5 sm:py-0 ${hideMenu ? "hidden": ""}  sm:grid `}>
           <div className="relative w-full">
             <button className="absolute right-0 h-full flex items-center px-[20px] border z-10 hover:cursor-pointer">
               <svg
@@ -36,7 +58,7 @@ function Header() {
             />
           </div>
         </div>
-        <div className="grow-3 grid items-center justify-items-end">
+        <div className={`grow items-center justify-items-end ${hideMenu ? "hidden": ""} sm:grid`}>
           <div className="w-[50%] relative h-full flex items-center gap-5">
             <img
               src={userIcon}
@@ -77,7 +99,9 @@ function Header() {
                   </div>
                 </div>
               </li>
-              <li className="break-words border-b py-2">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+              <li className="break-words border-b py-2">
+                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+              </li>
               <li className="break-words border-b py-2">
                 <a href="">Salir</a>
               </li>
