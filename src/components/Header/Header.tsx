58d0807f9b96701997ea8127820e86aea9c47f3b
jesustissera <jesustissera@gmail.com>
2025-05-22 23:36:25 -0300

    bloqueo elementos que por ahora no vamos a utilizar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,19 +3,19 @@ import { Link, NavLink } from "react-router-dom";
 import CartButton from "../CartButton/CartButton";
 
 function Header() {
-  const [hideMenuUser, setHideMenuUser] = useState(true);
+  const [hideMenuUser] = useState(true);
   const [hideMenuResponsive, setHideMenuResponsive] = useState(true);
 
-  const showHideMenuUser = () => {
+  /*const showHideMenuUser = () => {
     setHideMenuUser((prevState) => !prevState);
-  };
+  };*/
 
   const showHideMenuResponsive = () => {
     setHideMenuResponsive((prevState) => !prevState);
   };
 
   return (
-    <nav className="bg-gray-500 fixed top-0 w-full z-[10]">
+    <nav className="bg-gray-500 fixed top-0 w-full z-[20]">
       <div className="mx-auto container px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -87,7 +87,7 @@ function Header() {
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <CartButton />
-            <button
+            {/*<button
               type="button"
               className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
             >
@@ -108,10 +108,10 @@ function Header() {
                   d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
                 />
               </svg>
-            </button>
+            </button>*/}
 
             <div className="relative ml-3">
-              <div>
+              {/*<div>
                 <button
                   type="button"
                   className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
@@ -128,7 +128,7 @@ function Header() {
                     alt=""
                   />
                 </button>
-              </div>
+              </div>*/}
 
               <div
                 className={`absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-hidden ${
